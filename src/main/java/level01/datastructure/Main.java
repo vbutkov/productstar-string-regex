db7c4cadefca92a8648e00e5815c894c4eb5ba88
melchior <v.bytkov@yandex.ru>
2024-09-29 09:27:52 +0300

    added bubble sorting implementation

---
+++ src/main/java/level01/datastructure/Main.java
@@ -0,0 +1,57 @@
+package level01.datastructure;
+
+import java.util.Arrays;
+
+public class Main {
+
+    static int max_size = 2;
+    static int size = 0;
+    static int[] data = new int[max_size];
+
+    public static void main(String[] args) {
+        add(4);
+        add(3);
+        add(1);
+        add(5);
+        print(); // должно вывести 4,3,1,5
+        sort();
+        print(); // должно вывести 1,3,4,5
+    }
+
+    static void sort() {
+        for (int i = 1; i < data.length; i++) {
+            boolean sorted = true;
+            for (int j = 0; j < data.length - i; j++) {
+                if (data[j] > data[j + 1]) {
+                    int tmp = data[j];
+                    data[j] = data[j + 1];
+                    data[j + 1] = tmp;
+                    sorted = false;
+                }
+            }
+            if (sorted) return;
+        }
+    }
+
+    static void print() {
+        System.out.print("[");
+        for (int i = 0; i < data.length; i++) {
+            System.out.print(" " + data[i]);
+        }
+        System.out.println(" ]");
+    }
+
+    static void add(int value) {
+        if (size >= data.length) {
+            max_size *= 2;
+            int[] temp = Arrays.copyOf(data, size);
+            data = new int[max_size];
+            for (int i = 0; i < temp.length; i++) {
+                data[i] = temp[i];
+            }
+        }
+        data[size] = value;
+        size++;
+    }
+
+}
